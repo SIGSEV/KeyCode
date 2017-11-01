bebd4fad0d13dc6359492aca967e445934a06354
meriadec <meriadec.pillet@gmail.com>
2017-11-01 21:39:48 +0100

    ability to have action in button

--- src/actions/race.js
+++ src/actions/race.js
@@ -1,12 +1,13 @@
 import { initRace } from 'reducers/race'
 
 export function loadRace(id) {
-  return dispatch => {
-    console.log(`>>> loading race ${id}`)
+  return async dispatch => {
+    await new Promise(resolve => setTimeout(resolve, 1e3))
+    // TODO load the race
     dispatch(
       initRace({
         id,
-        text: 'hey there',
+        text: 'hey there blabaoentuhason thasontu hasoentuhason thasoetn',
       }),
     )
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,20 @@
 import React, { PureComponent } from 'react'
-import styled from 'styled-components'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import IconLoading from 'react-icons/lib/md/watch'
+import styled, { keyframes } from 'styled-components'
 import { Link as RouterLink } from 'react-router-dom'
 import { findDOMNode } from 'react-dom'
+import { Motion, spring } from 'react-motion'
 
-const BtnEl = props => <button {...props} />
+const BtnEl = ({ isLoading, ...props }) => <button disabled={isLoading} {...props} />
 
 /* eslint-disable no-unused-vars */
-const stylize = El => styled(({ accent, action, grey, ...props }) => <El {...props} />)`
+const stylize = El => styled(({ accent, push, action, grey, ...props }) => <El {...props} />)`
+  position: relative;
   display: inline-flex;
+  opacity: ${p => (p.isLoading ? 0.7 : 1)};
+  overflow: hidden;
   background: ${p =>
     p.accent ? p.theme.accent : p.grey ? p.theme.darkGrey02 : p.theme.darkGrey00};
   border-radius: 2px;
@@ -32,7 +39,7 @@ const stylize = El => styled(({ accent, action, grey, ...props }) => <El {...pro
     bottom: 0;
   }
 
-  &:hover:after {
+  &:hover:after:not(:disabled) {
     background: rgba(255, 255, 255, 0.05);
   }
 
@@ -56,20 +63,84 @@ const Wrapper = styled.div`
   text-shadow: rgba(0, 0, 0, 0.2) 0 1px 0;
 `
 
+const Loader = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const rotate = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(180deg); }
+`
+
+const Spinner = styled(IconLoading)`
+  animation: ${rotate} 250ms linear infinite;
+`
+
+const springConfig = {
+  stiffness: 200,
+  damping: 25,
+}
+
+@connect(null, {
+  push,
+})
 class Button extends PureComponent {
+  state = {
+    isLoading: false,
+  }
+
   focus() {
     const node = findDOMNode(this) // eslint-disable-line react/no-find-dom-node
     node.focus()
   }
+
+  handleAction = async () => {
+    const { action, to, push } = this.props
+    this.setState({ isLoading: true })
+    await action()
+    this.setState({ isLoading: false })
+    push(to)
+  }
+
   render() {
-    const { children, linkTo, ...props } = this.props
-    const content = <Wrapper>{children}</Wrapper>
-    return linkTo ? (
-      <Link to={linkTo} {...props}>
+    const { isLoading } = this.state
+    const { children, to, onClick, action, ...props } = this.props
+
+    const content = (
+      <Motion
+        style={{
+          offset: spring(isLoading ? 100 : 0, springConfig),
+        }}
+      >
+        {m => (
+          <Wrapper style={{ transform: `translate3d(${m.offset}%, 0, 0)` }}>{children}</Wrapper>
+        )}
+      </Motion>
+    )
+
+    const realOnClick = isLoading ? undefined : action ? this.handleAction : onClick
+    return to && !action ? (
+      <Link to={to} {...props}>
         {content}
       </Link>
     ) : (
-      <Btn {...props}>{content}</Btn>
+      <Btn onClick={realOnClick} isLoading={isLoading} {...props}>
+        <Motion style={{ offset: spring(isLoading ? 0 : -100, springConfig) }}>
+          {m => (
+            <Loader style={{ transform: `translate3d(${m.offset}%, 0, 0)` }}>
+              <Spinner size={24} />
+            </Loader>
+          )}
+        </Motion>
+        {content}
+      </Btn>
     )
   }
 }
