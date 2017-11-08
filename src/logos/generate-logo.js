4318f0954dfed13f7713ed56fa8ff0d9b691b098
meriadec <meriadec.pillet@gmail.com>
2017-11-08 20:45:36 +0100

    first drawings

--- src/logos/generate-logo.js
+++ src/logos/generate-logo.js
@@ -1,10 +1,52 @@
+const PIXEL_RATIO = (function() {
+  const ctx = document.createElement('canvas').getContext('2d')
+  const dpr = window.devicePixelRatio || 1
+  const bsr =
+    ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    ctx.backingStorePixelRatio ||
+    1
+  return dpr / bsr
+})()
+
+function createHiDPICanvas(size) {
+  const can = document.createElement('canvas')
+  can.width = size * PIXEL_RATIO
+  can.height = size * PIXEL_RATIO
+  can.style.width = `${size}px`
+  can.style.height = `${size}px`
+  can.getContext('2d').setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0)
+  return can
+}
+
 export default function generateLogo(num, opts = {}) {
   const { size = 90 } = opts
-  const canvas = document.createElement('canvas')
-  canvas.width = size
-  canvas.height = size
+  const canvas = createHiDPICanvas(size)
   const ctx = canvas.getContext('2d')
+
+  // white background
   ctx.fillStyle = 'white'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // K
+  const kSize = 80
+  // ctx.fillStyle = '#ddd'
+  ctx.fillStyle = 'rgba(22, 135, 238, 0.7)'
+  ctx.font = `${kSize}px "Inter UI Black"`
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'top'
+  ctx.fillText('K', size / 2 + 5, size / 2 - kSize / 2 - 5)
+
+  // num
+  const numSize = 40
+  ctx.fillStyle = 'black'
+  // ctx.fillStyle = '#1687ee'
+  ctx.font = `${numSize}px "Inter UI Black"`
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'top'
+  ctx.fillText(num, size / 2, size / 2 - numSize / 2)
+
   return canvas
 }
