e5f756ce12902d76050c7aea4fc3abb11fef4af1
Balthazar Gronon <balthazar@uber.com>
2017-11-22 14:35:23 -0800

    fix pricing height

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -28,7 +28,7 @@ const Choices = styled.div`
 `
 
 const Choice = styled.div`
-  height: 330px;
+  height: 400px;
   width: ${p => (p.isBig ? 300 : 250)}px;
   border-radius: 8px;
   border: 3px solid ${p => p.theme.darkGrey02};
