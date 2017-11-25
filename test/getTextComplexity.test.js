9d7765efd7aec2277c6e3aec2c82be698d757f91
meriadec <meriadec.pillet@gmail.com>
2017-11-25 20:13:28 +0100

    Revert "first attempts at calculating complexity"
    This reverts commit ff70e8eb968d24da4bb4a515eef43ce1361a60f2.
    because it was shit

--- test/getTextComplexity.test.js
+++
@@ -1,6 +0,0 @@
-import getTextComplexity from 'helpers/getTextComplexity'
-
-test('should detect a baby test', () => {
-  const text = 'aaa aaa aaa aaa'
-  expect(getTextComplexity(text)).toBe(1)
-})
